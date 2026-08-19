2b43a7ffb3a036cc4b6a8544fb856c18e0c63fe0
Lee Jia Sheng <leejiasheng0826@gmail.com>
2026-08-19 12:38:41 +0800

    fix: amend shadcn ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button"
 
 function App() {
-
   return (
-    <>
-      <h1>Hello World!!</h1>
-   </>
+    <main className="mx-auto flex min-h-svh max-w-5xl flex-col items-center justify-center gap-4 px-6 text-center">
+      <h1 className="font-heading text-4xl font-medium tracking-tight text-foreground">
+        Hello World!!
+      </h1>
+      <p className="text-muted-foreground">What is going on here!</p>
+      <Button className="rounded-md">Get started</Button>
+    </main>
   )
 }
 
